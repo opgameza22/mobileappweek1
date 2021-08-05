22f48402ab99b7e5c3f03806c49d9a9da244d330
opgameza22 <s6302041520040@email.kmutnb.ac.th>
2021-08-05 17:02:27 +0700

    Week2

---
+++ dart/Week2.dart
@@ -0,0 +1,35 @@
+void main() {
+  print(callGen(1999));
+  var x = callColor("Green");
+  print(x);
+  callWeb("Google",protocal: "https", port: 443);
+}
+
+String callGen(int year) =>  (year >= 1996) ? "Gen Z" : "Gen Y" ;
+
+  //var year = 1999;
+ /*   if(year >= 1996){
+      print("Gen Z");
+    }else{
+      print("Gen Y");
+    }*/
+    
+  // การเช็คเงื่อนไข               T          F
+   
+   int callColor([String newColor= "Pink" ]){
+     //                      0     1
+    List<dynamic> color = ["Red","Blue","Black"];
+
+    color.add(newColor);
+
+
+    for( var item in color ) {
+      print(item);
+    }
+
+    return color.length;
+   }
+
+//void callWeb([String web= "Facebook"]) => print("$web");
+void callWeb(String web,{int port = 80,String protocal = "http"}) => print("$protocal : $web:$port");
+
