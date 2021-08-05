3aa7576ca0b8ca389096439723c4c3472e01c035
opgameza22 <s6302041520040@email.kmutnb.ac.th>
2021-08-05 17:08:13 +0700

    Week2

--- dart/Week2.dart
+++ dart/Week2.dart
@@ -5,6 +5,7 @@ void main() {
   callWeb("Google",protocal: "https", port: 443);
 }
 
+//สร้าง Function การคำนวณ generation แบบง่ายโดยใช้การเขียนเงื่อนไขแบบ Short-hand
 String callGen(int year) =>  (year >= 1996) ? "Gen Z" : "Gen Y" ;
 
   //var year = 1999;
@@ -16,7 +17,7 @@ String callGen(int year) =>  (year >= 1996) ? "Gen Z" : "Gen Y" ;
     
   // การเช็คเงื่อนไข               T          F
 
-  
+//สร้าง Function การแสดงข้อมูลในตัวแปร List โดยใช้ For in และต้อง Return จำนวนข้อมูลใน List กลับด้วย
    int callColor([String newColor= "Pink" ]){
      //                      0     1
     List<dynamic> color = ["Red","Blue","Black"];
@@ -31,6 +32,8 @@ String callGen(int year) =>  (year >= 1996) ? "Gen Z" : "Gen Y" ;
     return color.length;
    }
 
+
+//สร้าง Function แสดงข้อมูลการเชื่อมต่อเว็บและPort แบบ Arrow และต้องกำลัง Parameter เป็นแบบ Optional ทั้ง Position และ Name
 //void callWeb([String web= "Facebook"]) => print("$web");
 void callWeb(String web,{int port = 80,String protocal = "http"}) => print("$protocal : $web:$port");
 
